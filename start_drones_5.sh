da739414947f8124b703ae6130923149036e0332
gecgelenus <44176143+gecgelenus@users.noreply.github.com>
2024-07-05 18:03:05 +0300

    Add files via upload

---
+++ start_drones_5.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Her MAVproxy oturumu başlattığımızda bir süre beklememiz gerekiyor. Aksi takdirde port'lar ile ilgili hata alırsınız.
+# Beklenmesi gereken süre bilgisayar hızına göre değişiklik gösterir
+
+
+gnome-terminal -- sh -c "sim_vehicle.py -v ArduCopter -f gazebo-iris --console -I0" # ilk drone için MAVproxy 
+sleep 1s
+gnome-terminal -- sh -c "sim_vehicle.py -v ArduCopter -f gazebo-iris --console -I1" # ikinci drone için MAVproxy 
+sleep 1s
+gnome-terminal -- sh -c "sim_vehicle.py -v ArduCopter -f gazebo-iris --console -I2" # üçüncü drone için MAVproxy 
+sleep 1s
+gnome-terminal -- sh -c "sim_vehicle.py -v ArduCopter -f gazebo-iris --console -I3" # dördüncü drone için MAVproxy 
+sleep 1s
+gnome-terminal -- sh -c "sim_vehicle.py -v ArduCopter -f gazebo-iris --console -I4" # beşinci drone için MAVproxy 
+
+# Gazebo çalışmadan önce MAVproxy oturumlarının başlamasını beklememiz gerekiyor. Aksi takdirde gazebo'da kullandığımız plugin'ler başlattığımız oturumları görmez.
+sleep 3s
+
+gazebo --verbose ../ardupilot_gazebo/worlds/iris_arducopter_runway_multi.world
